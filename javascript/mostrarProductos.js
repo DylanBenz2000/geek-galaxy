9be975e86558f309ac39cb54f22422fb8b307bfc
Dylan Ramos <dylanbenz2000@gmail.com>
2024-05-08 18:35:11 -0600

    Eliminacion de archivos de mostrarProducto, crearProducto, eliminarProducto

--- javascript/mostrarProductos.js
+++ javascript/mostrarProductos.js
@@ -1,32 +1,32 @@
-import { conexionAPI } from "./conexionAPI.js";
+// import { conexionAPI } from "./conexionAPI.js";
 
-const lista = document.querySelector('[data-lista]');
+// const lista = document.querySelector('[data-lista]');
 
-function crearCard(id,nombre,precio,imagen){
-    const producto = document.createElement("li");
-    producto.className = "card";
-    producto.dataset.id = id;
-    producto.innerHTML = `
-    <img src="${imagen}" alt="" class="card__image">
-    <div class="container-title">
-        <h3 class="card__title">${nombre}</h3>
-    </div>
-    <div class="price__trash">
-        <div class="price">
-            <i class="fa-solid fa-dollar-sign"></i>
-            <p class="precio__card">${precio}</p>
-        </div>
-        <i class="fa-regular fa-trash-can"></i>
-    </div>
-    `
+// function crearCard(id,nombre,precio,imagen){
+//     const producto = document.createElement("li");
+//     producto.className = "card";
+//     producto.dataset.id = id;
+//     producto.innerHTML = `
+//     <img src="${imagen}" alt="" class="card__image">
+//     <div class="container-title">
+//         <h3 class="card__title">${nombre}</h3>
+//     </div>
+//     <div class="price__trash">
+//         <div class="price">
+//             <i class="fa-solid fa-dollar-sign"></i>
+//             <p class="precio__card">${precio}</p>
+//         </div>
+//         <i class="fa-regular fa-trash-can"></i>
+//     </div>
+//     `
 
-    return producto;
-}
+//     return producto;
+// }
 
-async function listarProductos(){
-    const listaAPI = await conexionAPI.listarProductos();
-    // console.log(listaAPI);
-    listaAPI.forEach(producto=> lista.appendChild(crearCard(producto.id, producto.nombre, producto.precio, producto.imagen)))
-}
+// async function listarProductos(){
+//     const listaAPI = await conexionAPI.listarProductos();
+//     // console.log(listaAPI);
+//     listaAPI.forEach(producto=> lista.appendChild(crearCard(producto.id, producto.nombre, producto.precio, producto.imagen)))
+// }
 
-listarProductos();
+// listarProductos();
